e651cbfbfd61c804dfdbcd1d85e065c39baac04c
wrkbr <zcarc1@gmail.com>
2020-03-28 20:03:54 +0900

    #2 1 Local State part Two

--- src/apollo.js
+++ src/apollo.js
@@ -5,12 +5,27 @@ const client = new ApolloClient({
   // uri: 'https://movieql.now.sh'
   uri: "http://localhost:4000",
 
+
+  //백엔드의 resolvers 처럼 같은 역할
+  // Movie 처럼 저 자리에 올 수 있는 이름은 graphql 서버에 있는 schema의 type 이름과 똑같은 것에 추가해야 isLiked가 추가된다.
+  // 그러면 Movie가 호출될 때 isLiked라는 프로퍼티가 추가된다.
   resolvers: {
     Movie: {
-      isLiked: () => false,
+      isLiked: () => false
+    },
+
+    Mutation: {
+      likeMovie: (_, { id }, { cache }) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: true,
+            medium_cover_image: 'lalalala'
+          }
+        });
+      }
     }
   }
-  
 });
 
 export default client;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
+
+// @client를 붙이지 않으면 백엔드로 쿼리가 전송된다.
+const LIKE_MOVIE = gql`
+  mutation likeMovie($id: Int!) {
+    likeMovie(id: $id) @client
+  }
+`;
 
 const Container = styled.div`
   height: 400px;
@@ -19,11 +28,22 @@ const Poster = styled.div`
   border-radius: 7px;
 `;
 
-export default ({ id, bg, isLiked }) => (
-  <Container>
-    <Link to={`/${id}`}>
-      <Poster bg={`https://yst.am${bg}`} />
-    </Link>
-    <button>{isLiked ? "Unlike" : "Like"}</button>
-  </Container>
-);
+export default ({ id, bg, isLiked }) => {
+
+  const [likeMovie] = useMutation(LIKE_MOVIE, {
+    variables: { id: parseInt(id) }
+  });
+
+  // console.log("const [likeMovie]:", likeMovie);
+
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={`https://yst.am${bg}`} />
+      </Link>
+      <button onClick={isLiked ? null : likeMovie}>
+        {isLiked ? "Unlike" : "Like"}
+      </button>
+    </Container>
+  );
+};
