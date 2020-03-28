bed130f1196154e47c2bd9508d58c1e802296576
wrkbr <zcarc1@gmail.com>
2020-03-28 11:11:41 +0900

    #1 3 Apollo Cache and Styles

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useParams} from 'react-router-dom';
 import {gql} from 'apollo-boost';
 import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
 
 // gql의 getMovie() 라는 이름은 graphql 서버와는 관련이 없고
 // 아무렇게나 이름을 지어도 동작하는데는 이상이 없다.
@@ -10,13 +11,49 @@ const GET_MOVIE = gql`
   query getMovie($id: Int!) {
 
     movie(id: $id) {
-      id
       title
       medium_cover_image
+      language
+      rating
+      description_intro
     }
 
   }
 
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
 `;
 
 
@@ -28,15 +65,14 @@ export default () => {
         variables: {id: parseInt(id)}
     });
 
-    console.log(loading, data);
-
-    if(loading) {
-      return 'loading';
-    }
-
-    if(data && data.movie) {
-      return data.movie.title;
-    }
-
-    return 'Detail';
+    return (
+       <Container>
+         <Column>
+           <Title>Name</Title>
+           <Subtitle>English · 4.5</Subtitle>
+           <Description>lorem ipsum lalalla </Description>
+         </Column>
+         <Poster></Poster>
+       </Container>
+     );
 };
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -48,22 +48,38 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 
 export default () => {
 
     const { loading, error, data } = useQuery(GET_MOVIES);
     
     return (
-        <Container>
-            <Header>
-                <Title>Apollo 2020</Title>
-                <Subtitle>I love GraphQL</Subtitle>
-            </Header>
-            {loading && <Loading>Loading...</Loading>}
-            {!loading &&
-                data.movies &&
-                data.movies.map(m => <Movie key={m.id} id={m.id}/>)
-            }
-        </Container>
-    )
+      <Container>
+        <Header>
+          <Title>Apollo 2020</Title>
+          <Subtitle>I love GraphQL</Subtitle>
+        </Header>
+        {loading && <Loading>Loading...</Loading>}
+
+        {!loading && data.movies && (
+
+          <Movies>
+            {data.movies.map(m => (
+              <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+            ))}
+          </Movies>
+          
+        )}
+
+      </Container>
+    );
 };
